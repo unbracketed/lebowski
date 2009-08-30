71da06957f2886f1a5c7143b9d346657ca450ffc
Brian Luft <brian@unbracketed.com>
2009-08-30 01:00:46 -0700

    adds quotes

--- abide.py
+++ abide.py
@@ -1,7 +1,7 @@
 import random
 from itty import *
 from haiku import HAIKUS, FIVES, SEVENS
-
+from quotes import QUOTES
 
 @get('/')
 def index(request):
@@ -14,10 +14,13 @@ def haiku(request):
         return HAIKUS[random.randint(0,len(HAIKUS))]
     except IndexError:
         return "[THIS AGGRESSION WILL NOT STAND]"
-
+    
 
 @get('/quote')
 def quote(request):
-    return 'Hello World!'
+    try:
+        return QUOTES[random.randint(0,len(QUOTES))]
+    except:
+        return "[I AM THE WALRUS?]"
 
 run_itty()
--- quotes.py
+++ quotes.py
@@ -1,4 +1,10 @@
 'HTML snippets of selected quotes from "The Big Lebowski"'
 
 QUOTES = []
+f = open('quotes.txt')
+for line in f.readlines():
+    if not line.startswith('['):
+        QUOTES.append(line.strip())
+
+
 
